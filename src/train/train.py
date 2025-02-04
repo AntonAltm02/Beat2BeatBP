3d3b3e4f7221f89e03050f18245d2ccc6e19f5da
Anton Altmeyer <anton.altm@outlook.de>
2025-02-04 10:36:59 +0100

    - the file train.py holds the initial function train_basic_pat() to train a XGBoost model for predicting PAT types based on a feature type - the train files are in fact split into train and validation files

---
+++ src/train/train.py
@@ -0,0 +1,36 @@
+import sklearn
+from src.load import get_data
+import xgboost as xgb
+
+def train_basic_pat(files_train, pat_type, feature_type):
+    """
+    This function trains the XGBoost models either with basic features alone, ePAT alone or even with ePAT and features
+    in conjunction.
+    - If the train_type is set to "pat" in main.py, the model is trained using the desired
+    feature type as input and pat type as label or reference
+    - If the train_type is set to "bp" in main.py, the model is trained using the desired feature set alone, the
+    ePAT type alone or ePAT and feature set concatenated. This both for SBP and DBP estimation
+    """
+    print("Load Data")
+    files_train, files_val = sklearn.model_selection.train_test_split(files_train, test_size=0.1, random_state=0)
+    features_train, target_train, _, _ = get_data(files_train, pat_type=f"{pat_type}", feature_type=feature_type)
+    features_val, target_val, _, _ = get_data(files_val, pat_type=f"{pat_type}", feature_type=feature_type)
+    eval_set = [(features_train, target_train), (features_val, target_val)]
+    print("Data loaded \n")
+
+    print("Training the model")
+    num_boost_round = 10000
+    xgb_reg = xgb.XGBRegressor(
+        objective="reg:squarederror",
+        eval_metric="mae",
+        n_estimators=num_boost_round,
+        early_stopping_rounds=20,
+        learning_rate=0.01,
+        device="cpu",
+    )
+    xgb_reg.fit(features_train, target_train, eval_set=eval_set, verbose=True)
+    print("Training finished \n")
+
+    print("Saving the trained model")
+    # xgb_reg.save_model(f"{model_path}PAT/{model_name}")
+    print("Model saved \n")
