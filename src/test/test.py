e95e4645cf828f5f5008a1eae63da7a1b7f68289
Anton Altmeyer <anton.altm@outlook.de>
2025-02-04 11:00:13 +0100

    initial commit with test function

---
+++ src/test/test.py
@@ -0,0 +1,24 @@
+import xgboost as xgb
+from src.load import get_data
+import os
+
+script_dir = os.path.dirname(__file__)
+model_path = os.path.join(script_dir + "/model/")
+
+def test_basic_pat(files_test, pat_type, feature_type, model_name):
+    print("Load Model")
+    loaded_model = xgb.Booster()
+    loaded_model.load_model(model_path + model_name)
+    num_boosting_rounds = loaded_model.num_boosted_rounds()
+    print(f"Number of boosting rounds: {num_boosting_rounds}")
+    print("Model loaded \n")
+
+    print("Load Data")
+    features_test, pat_test, _, _ = get_data(files_test, pat_type=f"{pat_type}", feature_type=feature_type)
+    print("Data loaded \n")
+
+    print("Predicting the target variable")
+    dtest = xgb.DMatrix(features_test)
+    predictions = loaded_model.predict(data=dtest)
+    print("Prediction finished")
+    return predictions
