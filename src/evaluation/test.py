746cb87cb568372a1ae374ac8e738053ef4956c3
Anton Altmeyer <anton.altm@outlook.de>
2025-03-05 10:33:59 +0100

    functions to train and test BP XGBoost models

--- src/evaluation/test.py
+++ src/evaluation/test.py
@@ -1,5 +1,6 @@
+import numpy as np
 import xgboost as xgb
-from src.load import get_data
+from src.load import get_data, get_data_frame
 import os
 
 script_dir = os.path.dirname(__file__)
@@ -22,3 +23,17 @@ def test_basic_pat(files_test, pat_type, feature_type, model_name):
     predictions = loaded_model.predict(data=dtest)
     print("Prediction finished")
     return predictions, pat_test
+
+def test_basic_bp(features_test, target_test, bp_type, model_name):
+    print("Load Model")
+    loaded_model = xgb.Booster()
+    loaded_model.load_model(model_path + model_name)
+    num_boosting_rounds = loaded_model.num_boosted_rounds()
+    print(f"Number of boosting rounds: {num_boosting_rounds}")
+    print("Model loaded \n")
+
+    print("Predicting the target variable")
+    dtest = xgb.DMatrix(features_test)
+    predictions = loaded_model.predict(data=dtest)
+    print("Prediction finished")
+    return predictions, np.array(target_test)
--- src/train/train.py
+++ src/train/train.py
@@ -1,7 +1,8 @@
 import sklearn
-from src.load import get_data
 import xgboost as xgb
 import os
+from src.evaluation.eval import plot_hist
+from src.data.statistics import ks_test_bp
 
 def train_basic_pat(files_train, pat_type, feature_type, model_name):
     """
@@ -20,7 +21,7 @@ def train_basic_pat(files_train, pat_type, feature_type, model_name):
     print("Data loaded \n")
 
     print("Training the model")
-    num_boost_round = 1000
+    num_boost_round = 5000
     xgb_reg = xgb.XGBRegressor(
         objective="reg:squarederror",
         eval_metric="mae",
@@ -37,3 +38,33 @@ def train_basic_pat(files_train, pat_type, feature_type, model_name):
     model_path = os.path.join(script_dir + "/../model/")
     xgb_reg.save_model(model_path + model_name)
     print("Model saved \n")
+
+def train_basic_bp(features_train, features_val, target_train, target_val, bp_type, model_name):
+    """
+    This function trains the XGBoost models either with basic features alone, ePAT alone or even with ePAT and features
+    in conjunction.
+    - If the train_type is set to "pat" in main.py, the model is trained using the desired
+    feature type as input and pat type as label or reference
+    - If the train_type is set to "bp" in main.py, the model is trained using the desired feature set alone, the
+    ePAT type alone or ePAT and feature set concatenated. This both for SBP and DBP estimation
+    """
+
+    eval_set = [(features_train, target_train), (features_val, target_val)]
+    plot_hist(target_train, target_val)
+
+    print("Training the model")
+    num_boost_round = 1000
+    xgb_reg = xgb.XGBRegressor(
+        objective="reg:squarederror",
+        eval_metric="mae",
+        n_estimators=num_boost_round,
+        early_stopping_rounds=20,
+        learning_rate=0.01,
+        device="cpu",
+    )
+    xgb_reg.fit(features_train, target_train, eval_set=eval_set, verbose=True)
+
+    print("Saving the trained model")
+    script_dir = os.path.dirname(__file__)
+    model_path = os.path.join(script_dir + "/../model/")
+    xgb_reg.save_model(model_path + model_name)
