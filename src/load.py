b2a9003ba51b7c073430726959002617f6ef1b2f
Anton Altmeyer <anton.altm@outlook.de>
2025-03-05 10:26:34 +0100

    - included a function get_data_frame() to load a dataframe of all features, PAT and BP - the function process() gets the data frames and processes the data as features and target data (!!! This needs to be refined for both BP and PAT estimation)

--- src/load.py
+++ src/load.py
@@ -1,12 +1,13 @@
 from tqdm import tqdm
 import numpy as np
 import os
+import pandas as pd
 
 script_dir = os.path.dirname(__file__)
 data_path = os.path.join(script_dir + "/../data/processed/")
 
 
-def get_data(files, pat_type, feature_type):
+def get_data(files, pat_type=None, feature_type=None):
     features_list = []
     pat_list = []
     sbp_list = []
@@ -29,3 +30,25 @@ def get_data(files, pat_type, feature_type):
     dbp = dbp[non_zero_rows]
 
     return features, pat, sbp, dbp
+
+def get_data_frame(files):
+    result_df = pd.DataFrame()
+    for file in files:
+        df = pd.read_csv(data_path + f"DataFrame/{file[:10]}.csv")
+        df = df.drop(columns=["Unnamed: 0"])
+        result_df = pd.concat([result_df, df], ignore_index=True)
+    return result_df
+
+def process(files_train, files_val, files_test):
+    df_train = get_data_frame(files_train)
+    df_val = get_data_frame(files_val)
+    df_test = get_data_frame(files_test)
+    drop_columns = ["FinapresSBP", "FinapresDBP", "FinapresPP", "PPG", "SBP", "DBP", "PP", "MBP",
+                    "PAT(ON)", "PAT(U)", "PAT(IT)", "PAT(SP)", "PAT(DN)", "PAT(DP)"]
+    features_train = df_train.drop(columns=drop_columns)
+    features_val = df_val.drop(columns=drop_columns)
+    features_test = df_test.drop(columns=drop_columns)
+    target_train = df_train[f"Finapres{bp_type.upper()}"]
+    target_val = df_val[f"Finapres{bp_type.upper()}"]
+    target_test = df_test[f"Finapres{bp_type.upper()}"]
+    return features_train, features_val, features_test, target_train, target_val, target_test
